0ba7dd921c83cc451e0df82b07f99a682dd8333e
Gabriel Nóbrega <gaabriel_noobrega@hotmail.com>
2025-08-25 16:14:26 -0300

    Undestand run on git

--- hooks.js
+++ hooks.js
@@ -24,6 +24,10 @@ BeforeAll(function () {
         CleanFolder(paths.screenshotsDir)
         CleanFolder(paths.dataVideosDir)
     }
+    console.log('ls e2e/Tests/Features:', require('fs').readdirSync('e2e/Tests/Features').join('\n'))
+    console.log('steps exists?', require('fs').existsSync('e2e/Tests/Steps/Steps.mjs'))
+    console.log(`Running with CUCUMBER_WORKER_ID=${process.env.CUCUMBER_WORKER_ID || 'not defined'}`)
+
 })
 
 Before(async function () {
